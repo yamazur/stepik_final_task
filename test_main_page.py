b4d119ed9715ccca2d52546cf9a9b52a9c394b3e
Yana Mazur <mazurihaa@yandex.ru>
2025-12-04 22:59:30 +0300

    The first prototype of the page has been implemented

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,11 @@
 from selenium.webdriver.common.by import By
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-   browser.get(link)
-   go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open() # открываем страницу
+    page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
 
-class MainPage
+
+
